a14029f30b78069130bc76668cb877816f418a56
uimarshall <uimarshall@gmail.com>
2019-01-18 16:39:30 +0100

    added files views and partials

--- TemplateEngines/app.js
+++ TemplateEngines/app.js
@@ -1,16 +1,36 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.get('/', (req, res)=>{
-	res.render('home.ejs')
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+	res.render("home", { title: "" });
+});
+app.get("/posts", (req, res) => {
+	posts = [
+		{
+			title: "Ronaldo wins again",
+			author: "Delaney"
+		},
+		{
+			title: "Election violence",
+			author: "Oittoju"
+		},
+		{
+			title: "A new nobel laureate",
+			author: "Fisher"
+		}
+	];
+	res.render("post", { posts: posts, title: "posts" });
 });
 
-app.get('/players/:name', (req, res)=>{
+app.get("/players/:name", (req, res) => {
 	let routeVar = req.params.name;
-	res.render('embedjs.ejs', {dataTobeEmbed: routeVar})
+	res.render("embedjs", { dataTobeEmbed: routeVar, title: "players" });
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=>{
-	console.log(`Server is listening on port ${PORT}` );
-})
+app.listen(PORT, () => {
+	console.log(`Server is listening on port ${PORT}`);
+});
