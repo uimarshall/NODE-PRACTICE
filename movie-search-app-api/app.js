8bb128e1cf26f653c85dc8b4ea32cd614aaed4e0
uimarshall <uimarshall@gmail.com>
2019-01-21 17:18:59 +0100

    added more files for node api

--- movie-search-app-api/app.js
+++ movie-search-app-api/app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const request = require("request");
 const app = express();
 app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+	res.render("search");
+});
 app.get("/results", (req, res) => {
-	request("http://www.omdbapi.com/?apikey=5f985ba9&s=Batman", function(
-		error,
-		response,
-		body
-	) {
+	console.log(typeof req.query.search);
+	let query = req.query.search;
+	let url = "http://www.omdbapi.com/?apikey=5f985ba9&s=" + query;
+	request(url, function(error, response, body) {
 		if (!error && response.statusCode == 200) {
 			let apiCalldata = JSON.parse(body);
 			res.render("apiCallResults", { data: apiCalldata });
